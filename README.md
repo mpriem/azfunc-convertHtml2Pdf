c94b119927aaf8dfa9eec63f3e01a5ce5595a2ff
Mark Priem <mark.priem@outlook.com>
2021-05-07 15:44:09 +0200

    Final commit

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ https://adityadeshpandeadi.wordpress.com/2020/02/02/how-to-run-a-windows-executa
 
 1. Download the binary of choice from https://wkhtmltopdf.org/ to the root of the function project (next to host.json). You can use linux, windows, 32-bit, 64-bit depending on your Function App hosting environment.
 2. Ensure the project definition is aware of the executable (check out the csproj file in this project for reference).
-3. Deploy your Function app.
+3. Deploy your Function app. **Ensure you are using at least a Basic pricing tier for your App Hosting Plan** There are limitations to what you can do on Consumption plan sandboxes. Your processes will hang for no apparent reason. This has to do with resource consumption and limited access to lower level API's.
 4. Use HTTP POST in your integration solution (for example the HTTP action in your Logic App) and paste the raw HTML as part of the body to your Function.
 5. Convert the Base64 encoded binary from the body of the response to an PDF.
 
--- convert-Html2Pdf.cs
+++ convert-Html2Pdf.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -18,64 +20,111 @@ namespace Qoders.Az.Functions
         [FunctionName("convert-Html2Pdf")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log, ExecutionContext context)
+            ILogger log, Microsoft.Azure.WebJobs.ExecutionContext context)
         {
-            string TempFolder = Environment.GetEnvironmentVariable("TMP");
+            log.LogInformation($"Starting {context.FunctionName}");
+            string WorkDir = Environment.GetEnvironmentVariable("TMP");
             string ExeLocation = $"{context.FunctionAppDirectory}\\wkhtmltopdf.exe";
             string base64FileString = "";
-            string output = "";
+            string guid = Guid.NewGuid().ToString();
+            string htmlFile = $"{WorkDir}\\{guid}.htm";
+            string pdfFile = $"{WorkDir}\\{guid}.pdf";
+
             try
             {
-                //Ensure unique names
-                string guid = Guid.NewGuid().ToString();
-                string htmlFile = $"{TempFolder}\\{guid}.htm";
-                string pdfFile = $"{TempFolder}\\{guid}.pdf";
+                if(!File.Exists(ExeLocation)){
+                    throw new FileNotFoundException($"{ExeLocation} does not exist");
+                }
 
-                //Write HTML file to TMP folder
                 string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                log.LogDebug($"Body: `n {requestBody}");
                 File.WriteAllText(htmlFile, requestBody);
 
-                //Run process to convert the HTML file to PDF
-                ProcessStartInfo info = new ProcessStartInfo
+                log.LogInformation($"HTML File created: {File.Exists(htmlFile)}");
+
+                log.LogInformation("Starting pdf conversion");
+                int timeout = 30000;
+                using (Process process = new Process())
                 {
-                    WorkingDirectory = TempFolder,
-                    FileName = ExeLocation,
-                    Arguments = $" {htmlFile} {pdfFile}",
-                    WindowStyle = ProcessWindowStyle.Minimized,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                };
-                Process proc = new Process
-                {
-                    StartInfo = info
-                };
-                proc.Refresh();
-                proc.StartInfo.RedirectStandardOutput = true;
-                proc.Start();
-                //If any output write to logs
-                while (!proc.StandardOutput.EndOfStream)
-                {
-                    output = proc.StandardOutput.ReadLine();
+                    ProcessStartInfo pInfo = new ProcessStartInfo
+                    {
+                        WorkingDirectory = WorkDir,
+                        FileName = ExeLocation,
+                        Arguments = $"-q {htmlFile} {pdfFile}",
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        RedirectStandardError = true,
+                        RedirectStandardOutput = true,
+                        RedirectStandardInput = false
+                    };
+                    process.StartInfo = pInfo;
+
+                    StringBuilder output = new StringBuilder();
+                    StringBuilder error = new StringBuilder();
+
+                    //event handlers to handle outputs
+                    using (AutoResetEvent outputWaitHandle = new AutoResetEvent(false))
+                    using (AutoResetEvent errorWaitHandle = new AutoResetEvent(false))
+                    {
+                        process.OutputDataReceived += (sender, e) => {
+                            if (e.Data == null)
+                            {
+                                outputWaitHandle.Set();
+                            }
+                            else
+                            {
+                                output.AppendLine(e.Data);
+                            }
+                        };
+                        process.ErrorDataReceived += (sender, e) =>
+                        {
+                            if (e.Data == null)
+                            {
+                                errorWaitHandle.Set();
+                            }
+                            else
+                            {
+                                error.AppendLine(e.Data);
+                            }
+                        };
+
+                        process.Start();
+
+                        // start reading output streams
+                        process.BeginOutputReadLine();
+                        process.BeginErrorReadLine();
+
+                        if (process.WaitForExit(timeout) &&
+                            outputWaitHandle.WaitOne(timeout) &&
+                            errorWaitHandle.WaitOne(timeout))
+                        {
+                            log.LogInformation($"PDF File created: {File.Exists(pdfFile)}");
+                            if(!File.Exists(pdfFile)){
+                                throw new FileNotFoundException($"{pdfFile} not found");
+                            }
+                            var fileBytes = File.ReadAllBytes(pdfFile);
+                            base64FileString = Convert.ToBase64String(fileBytes);
+                        }
+                        else
+                        {
+                            throw new Exception("Operation timed out");
+                        }
+                    }
+                    log.LogInformation($"Output: {output.ToString()}");
+                    log.LogError($"Errors: {error.ToString()}");
                 }
-                log.LogInformation(output);
-                //Wait for process exit.
-                proc.WaitForExit();
-                
-                //Read the PDF file and convert to Base64 string
-                var fileBytes = File.ReadAllBytes(pdfFile);
-                base64FileString = Convert.ToBase64String(fileBytes);
-
-
             }
             catch (Exception e)
             {
                 log.LogError(e.Message);
                 return (ActionResult)new StatusCodeResult(500);
             }
-            
-            //Include base64 string in result
+            finally{
+                if(File.Exists(htmlFile)){ File.Delete(htmlFile); } 
+                if(File.Exists(pdfFile)){ File.Delete(pdfFile); } 
+                log.LogInformation($"Stopping {context.FunctionName}");
+            }
             return (ActionResult)new OkObjectResult(base64FileString);
-
         }
     }
 }
